b768068c75f4a578eed67e61ceea12a2dee310af
Meghana <meghanakongara0429@gmail.com>
2025-11-26 07:30:40 +0530

    Added

--- backend/app.py
+++ backend/app.py
@@ -7,13 +7,7 @@ from io import BytesIO
 
 app = Flask(__name__)
 
-CORS(app, resources={
-    r"/*": {
-        "origins": "*",
-        "methods": ["POST"],
-        "allow_headers": ["Content-Type"],
-    }
-})
+CORS(app, resources={r"/predict": {"origins": "*"}}, supports_credentials=True)
 
 
 # Load your trained model
